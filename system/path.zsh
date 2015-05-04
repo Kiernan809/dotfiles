d68f24945af4d6627414b658d26b4d5d4770cdb5
Kiernan809 <lee.kiernan@gmail.com>
2015-05-05 00:01:13 +0100

    Make omz and zsh play nice...

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,8 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSHELL/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export NVM_DIR="/Users/leekiernan/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
